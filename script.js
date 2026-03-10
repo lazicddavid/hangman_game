b2657c718a0fc0ccb7e57744e13effbed5b7386f
David Lazic <lazic.ddavid@gmail.com>
2026-03-10 21:16:19 +0100

    Added gameState

---
+++ script.js
@@ -0,0 +1,18 @@
+const words = ["......"];
+
+const categories = {
+  movies: ["gladiator", "titanic"],
+  sport: ["Football", "box"],
+  countries: ["Serbia", "France", "spain"],
+  capitals: ["belgrade", "paris", "tokyo", "madrid"],
+  animals: ["cat", "dog", "shark"],
+};
+
+const gameState = {
+  category: "",
+  chosenWord: "",
+  guessedLetters: [],
+  wrongLetter: [],
+};
+
+//funkcija koja bira rec
