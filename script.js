d4bf6735b73d40073458a47a6c8a7132dfe93dc6
David Lazic <lazic.ddavid@gmail.com>
2026-03-15 19:28:57 +0100

    Added classlist

--- script.js
+++ script.js
@@ -3,22 +3,17 @@ const DOM = {
   backBtn: document.getElementById("backBtn"),
   startScreen: document.getElementById("startScreen"),
   categoryScreen: document.getElementById("categoryScreen"),
+  gameScreen: document.getElementById("gameScreen"),
 };
 
 const words = ["......"];
 
 const categories = {
-  movies: ["gladiator", "titanic"],
-  sport: ["Football", "box"],
-  countries: ["Serbia", "France", "spain"],
-  capitals: ["belgrade", "paris", "tokyo", "madrid"],
-  animals: ["cat", "dog", "shark"],
-};
-
-const wordsByCategory = {
-  movies: ["TITANIC", "GLADIATOR", "INCEPTION"],
-  sport: ["TENIS", "FUDBAL", "KOSARKA"],
-  countries: ["SRBIA", "FRANCE", "BRAZIL"],
+  movies: ["GLADIATOR", "TITANIC"],
+  sport: ["FOOTBALL", "BOX"],
+  countries: ["SERBIA", "FRANCE", "SPAIN"],
+  capitals: ["BELGRADE", "PARIS", "TOKYO", "MADRID"],
+  animals: ["CAT", "DOG", "SHARK"],
 };
 
 const gameState = {
@@ -41,18 +36,18 @@ DOM.backBtn.addEventListener("click", function () {
 });
 
 //funkcije:
-
+/*
 let selectedCategory = "";
 let selectedWord = "";
-
+*/
 function openGame(category) {
   selectedCategory = category;
 
   const words = categories[category];
   selectedWord = words[Math.floor(Math.random() * words.length)];
 
-  categoryScreen.classList.add("hidden");
-  gameScreen.classList.remove("hidden");
+  DOM.categoryScreen.classList.add("hidden");
+  DOM.gameScreen.classList.remove("hidden");
 
   console.log(selectedCategory);
   console.log(selectedWord);
